1ae59bd30f0bed08d64d92b510e02c1816849f60
kamisatomasatoshi <k021g1271@g.neec.ac.jp>
2022-12-05 10:13:44 +0900

    add

--- main.cpp
+++ main.cpp
@@ -11,9 +11,44 @@ typedef struct cell {
 
 }CELL;
 
-void create(CELL* currentCell, int val); //プロトタイプ宣言
-void index(CELL* endCell);
-CELL* getInsertCellAddress(CELL* endCELL, int iterator);
+void create(CELL* currentCell, int val) {
+	CELL* newCell;
+	newCell = (CELL*)malloc(sizeof(CELL));
+	newCell->val = val;
+	newCell->prev = currentCell;
+	newCell->next = currentCell->next;
+
+	if (currentCell->next) {
+		CELL* newCell = currentCell->next;
+		newCell->prev = newCell;
+	}
+	currentCell->next = newCell;
+}
+
+void index(CELL* endCell) {
+	int no = 1;
+	while (endCell->next != nullptr) {
+		endCell = endCell->next;
+		printf("%d ", no);
+		printf("%p ", endCell->prev);
+		printf("%5d ", endCell->val);
+		printf("(%p) ", endCell);
+		printf("%p\n ", endCell->next);
+		no++;
+	}
+
+}
+CELL* getInsertCellAddress(CELL* endCELL, int iterator) {
+	for (int i = 0; i < iterator; i++) {
+		if (endCELL->next) {
+			endCELL = endCELL->next;
+		}
+		else {
+			break;
+		}
+	}
+	return endCELL;
+}
 
 int main()
 {
@@ -42,43 +77,8 @@ int main()
 	return 0;
 }
 
-void create(CELL* currentCell, int val) {
-	CELL* newCell;
-	newCell = (CELL*)malloc(sizeof(CELL));
-	newCell->val = val;
-	newCell->prev = currentCell;
-	newCell->next = currentCell->next;
-
-	if (currentCell->next) {
-		CELL* newCell = currentCell->next;
-		newCell->prev = newCell;
-	}
-	currentCell->next = newCell;
-}
 
 
-void index(CELL* endCell) {
-	int no = 1;
-	while (endCell->next != nullptr) {
-		endCell = endCell->next;
-		printf("%d ", no);
-		printf("%p ", endCell->prev);
-		printf("%5d ", endCell->val);
-		printf("(%p) ", endCell);
-		printf("%p\n ", endCell->next);
-		no++;
-	}
 
-}
 
-CELL* getInsertCellAddress(CELL* endCELL, int iterator) {
-	for (int i = 0; i < iterator; i++) {
-		if (endCELL->next) {
-			endCELL = endCELL->next;
-		}
-		else {
-			break;
-		}
-	}
-	return endCELL;
-}
+
